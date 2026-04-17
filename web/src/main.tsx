c304dcaadd19e6cdf8156be60d66747d4f1fec2c
Aly Raffauf <aly@raffauflabs.com>
2026-04-17 11:42:16 -0400

    fix caching (2)

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -5,6 +5,10 @@ import { router } from "./router/routes";
 import { BreadcrumbProvider } from "./hooks/useBreadcrumb";
 import "./index.css";
 
+window.addEventListener("pageshow", (event) => {
+  if (event.persisted) window.location.reload();
+});
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BreadcrumbProvider>
