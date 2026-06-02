7f2ee95d0f498946eb3412cec90e73635a275d8d
Aly Raffauf <aly@aly.codes>
2026-06-02 11:46:34 -0400

    add flake

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  description = "atbbs forums on atproto";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0";
+
+  outputs = {self, ...} @ inputs: let
+    inherit (inputs.nixpkgs) lib;
+
+    supportedSystems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "aarch64-darwin"
+    ];
+
+    forEachSupportedSystem = f:
+      lib.genAttrs supportedSystems (
+        system:
+          f {
+            inherit system;
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              config.allowUnfree = true;
+            };
+          }
+      );
+  in {
+    formatter = forEachSupportedSystem ({pkgs, ...}: pkgs.alejandra);
+
+    packages = forEachSupportedSystem (
+      {pkgs, ...}: {
+        default = pkgs.python3Packages.buildPythonPackage {
+          pname = "atbbs";
+          version = "dev";
+          src = ./.;
+          pyproject = true;
+          build-system = [pkgs.python3Packages.hatchling];
+
+          dependencies = with pkgs.python3Packages; [
+            aiohttp
+            authlib
+            httpx
+            piexif
+            platformdirs
+            rich-click
+            textual
+          ];
+        };
+      }
+    );
+  };
+}
